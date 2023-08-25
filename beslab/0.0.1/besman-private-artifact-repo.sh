99fbace9013870b04ac17af2580c39f5d9313b75
Arun Suresh <58501088+asa1997@users.noreply.github.com>
2023-08-25 12:02:58 +0530

    Update besman-private-artifact-repo.sh

--- beslab/0.0.1/besman-private-artifact-repo.sh
+++ beslab/0.0.1/besman-private-artifact-repo.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 function __besman_install_jfrog()
 {
+    #TODO- Develop the function
     echo "Installing jfrog"
 }
 
 function __besman_uninstall_jfrog()
 {
+    #TODO- Develop the function
     echo "Uninstalling jfrog"
 }
