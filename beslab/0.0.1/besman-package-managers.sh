5b1e3317e2cf32511ee1bb08653c30332dd430c3
Arun Suresh <58501088+asa1997@users.noreply.github.com>
2023-08-25 12:02:22 +0530

    Update besman-package-managers.sh

--- beslab/0.0.1/besman-package-managers.sh
+++ beslab/0.0.1/besman-package-managers.sh
@@ -14,35 +14,4 @@ function __besman_uninstall_package_managers()
     sudo apt purge --autoremove nodejs npm gradle maven python3-pip
 }
 
-# function __besman_install_npm()
-# {
-#     [[ -n $(which npm) ]] && __besman_echo_yellow "npm found" && return 
-#     __besman_echo_yellow "Installing npm"
-#     sudo apt update
-#     sudo apt install nodejs
-#     sudo apt install npm
-# }
 
-# function __besman_install_maven()
-# {
-#     [[ -n $(which maven) ]] && __besman_echo_yellow "maven found" && return 
-#     __besman_echo_yellow "Installing maven"
-#     sudo apt update
-#     sudo apt install maven
-# }
-
-# function __besman_install_gradle()
-# {
-#     [[ -n $(which gralde) ]] && __besman_echo_yellow "gralde found" && return 
-#     __besman_echo_yellow "Installing gradle"
-#     sudo apt update
-#     sudo apt install gralde
-# }
-
-# function __besman_install_pip()
-# {
-#     [[ -n $(which pip) ]] && __besman_echo_yellow "pip found" && return 
-#     __besman_echo_yellow "Installing pip"
-#     sudo apt update
-#     sudo apt install python3-pip
-# }
